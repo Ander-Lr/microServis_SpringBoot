cbadb1301f924883b1dff019706539b20c36a477
Ander-Lr <andersonllr4@gmail.com>
2024-07-02 22:38:45 -0500

    Initial commit

---
+++ src/test/java/ec/edu/api/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
